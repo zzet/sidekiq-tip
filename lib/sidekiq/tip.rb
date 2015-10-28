b5db6c085ffcf58afd8dc43be9cfaa2220b535cd
Andrew Kumanyaev <me@zzet.org>
2015-10-28 18:20:11 +0300

    Remove pry

--- lib/sidekiq/tip.rb
+++ lib/sidekiq/tip.rb
@@ -1,6 +1,5 @@
 require "sidekiq/tip/version"
 require "sidekiq/tip/server/middleware"
-require 'pry'
 
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
--- lib/sidekiq/tip/server/middleware.rb
+++ lib/sidekiq/tip/server/middleware.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 module Sidekiq
   module Tip
     module Server
